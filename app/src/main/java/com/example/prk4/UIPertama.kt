d8b1148b6ae6032ca5aae686c5e5b7b078d1799f
naufalbayazid <naufalichwanbayazid@gmail.com>
2025-10-24 10:14:03 +0700

    menambahkan spacer

--- app/src/main/java/com/example/prk4/UIPertama.kt
+++ app/src/main/java/com/example/prk4/UIPertama.kt
@@ -63,6 +63,8 @@ fun ActivitasPertama(modifier: Modifier){
                     modifier = Modifier
                         .size(size = 100.dp)
                         .padding(all = 5.dp)
+                )
+                Spacer(modifier = Modifier.width( width = 30.dp))
 
 
 
