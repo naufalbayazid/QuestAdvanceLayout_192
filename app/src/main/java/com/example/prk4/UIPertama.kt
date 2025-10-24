9984c388f194bd01aec79c820c60a99ba9492a11
naufalbayazid <naufalichwanbayazid@gmail.com>
2025-10-24 10:03:31 +0700

    membuat string baru prodi dan univ

--- app/src/main/java/com/example/prk4/UIPertama.kt
+++ app/src/main/java/com/example/prk4/UIPertama.kt
@@ -1,24 +1,46 @@
-package com.example.prk4
+package com.example.praktikum3
 
+import android.R.attr.text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import java.lang.reflect.Modifier
+import androidx.compose.ui.unit.sp
+import com.example.prk4.R
+
 
 @Composable
-fun ActivityPertama(modifier: Modifier){
-    Column (
+fun ActivitasPertama(modifier: Modifier){
+    Column(
         modifier = Modifier
             .padding(top = 100.dp)
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ){
         Text(
-            text = stringResource(id=R.string.prodi),
+            text = stringResource( id = R.string.prodi),
             fontSize = 35.sp,
             fontWeight = FontWeight.Bold
         )
-    }
-}
+        Text(
+            text = stringResource( id = R.string.univ),
+            fontSize = 22.sp
