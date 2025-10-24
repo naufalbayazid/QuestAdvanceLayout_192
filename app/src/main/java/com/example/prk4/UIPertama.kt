1e1c600d479e5fcf1902aa6128c0a4f97e8bedc1
naufalbayazid <naufalichwanbayazid@gmail.com>
2025-10-24 09:12:14 +0700

    mengatur paddig dan memasukkan CenterHorizontally

---
+++ app/src/main/java/com/example/prk4/UIPertama.kt
@@ -0,0 +1,16 @@
+package com.example.prk4
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+import java.lang.reflect.Modifier
+
+@Composable
+fun ActivitasPertama(modifier: Modifier){
+    Column(
+        modifier = Modifier
+            .padding(top = 100.dp)
+            .fillMaxsize(),
+        horizontalAlignment.CenterHorizontally
+
+    )
+}
